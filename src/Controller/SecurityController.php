b814ee0e9713d540dd5fef59d0465ee6be23c32d
Eddy <eddyjohn.terosier@hotmail.com>
2023-01-25 12:10:15 +0100

    Login/logout terminé a voir banniere2

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login', methods :['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('security/login.html.twig', [
+            'last_username' => $authenticationUtils->getLastUsername(),
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_logout')]
+    public function logout()
+    {
+
+    }
+}
