532aefa5d5b1ed5c5b01fb0e967fe33a9b6dcc16
Eddy <eddyjohn.terosier@hotmail.com>
2023-02-02 18:26:11 +0100

    Changements mineurs

--- public/assets/js/animation.js
+++ public/assets/js/animation.js
@@ -14,60 +14,71 @@ function checkboxes() {
   })
 }
 
-window.addEventListener("scroll", checkboxes1)
+// window.addEventListener("scroll", checkboxes1)
 
-function checkboxes1() {
-  const boxes = document.querySelectorAll(".box1")
-  const triggerBottom = window.innerHeight / 4 * 3;
-  boxes.forEach(box => {
-    const boxTop = box.getBoundingClientRect().top
+// function checkboxes1() {
+//   const boxes = document.querySelectorAll(".box1")
+//   const triggerBottom = window.innerHeight / 4 * 3;
+//   boxes.forEach(box => {
+//     const boxTop = box.getBoundingClientRect().top
 
-    if (boxTop< triggerBottom) {
-      box.classList.add("show")
-    } else {
-      box.classList.remove("show")
-    }
-  })
-}
+//     if (boxTop< triggerBottom) {
+//       box.classList.add("show")
+//     } else {
+//       box.classList.remove("show")
+//     }
+//   })
+// }
 
-const activeState = (e)=>{
-  const id = e.target.id
-  const idArray = ["1","2","3","4","5"]
+// const activeState = (e)=>{
+//   const id = e.target.id
+//   const idArray = ["1","2","3","4","5"]
 
-  idArray.forEach((element)=> {
-      document.getElementById(element).classList.remove("active")
-  });
-  document.getElementById(id).classList.add("active")
-}
+//   idArray.forEach((element)=> {
+//       document.getElementById(element).classList.remove("active")
+//   });
+//   document.getElementById(id).classList.add("active")
+// }
 
-function activeAccueil() {
-  const buttons = document.querySelectorAll("nav")
-  const id = 1;
-  const idArray = ["0","1","2","3"]
+// function activeAccueil() {
+//   const buttons = document.querySelectorAll("nav")
+//   const id = 1;
+//   const idArray = ["0","1","2","3"]
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", function() {
-      idArray.forEach((element) => {
-        document.getElementById(element).classList.remove("active");
-      });
-      document.getElementById(id).classList.add("active");
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", function() {
+//       idArray.forEach((element) => {
+//         document.getElementById(element).classList.remove("active");
+//       });
+//       document.getElementById(id).classList.add("active");
+//     });
+//   });
+
+//   idArray.forEach((element)=> {
+//       document.getElementById(element).classList.remove("active")
+//   });
+//   document.getElementById(id).classList.add("active");
+// }
+
+// const buttons = document.querySelectorAll("nav");
+// const idArray = ["1", "2", "3", "4", "5"];
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", function() {
+//     idArray.forEach((element) => {
+//       document.getElementById(element).classList.remove("active");
+//     });
+//     this.classList.add("active");
+//   });
+// });
+function toggleActiveClass() {
+  const navLinks = document.querySelectorAll('.navbar-nav a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      navLinks.forEach(link => link.classList.remove('active'));
+      this.classList.add('active');
     });
   });
-
-  idArray.forEach((element)=> {
-      document.getElementById(element).classList.remove("active")
-  });
-  document.getElementById(id).classList.add("active");
 }
 
-const buttons = document.querySelectorAll("nav");
-const idArray = ["1", "2", "3", "4", "5"];
-
-buttons.forEach((button) => {
-  button.addEventListener("click", function() {
-    idArray.forEach((element) => {
-      document.getElementById(element).classList.remove("active");
-    });
-    this.classList.add("active");
-  });
-});
+document.addEventListener('DOMContentLoaded', toggleActiveClass);
