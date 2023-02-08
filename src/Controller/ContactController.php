ceccf038ccdc015f468f44fd45af0fdd3327f067
Eddy <eddyjohn.terosier@hotmail.com>
2023-02-08 11:31:55 +0100

    bug mailtrap

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,21 +5,27 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request, EntityManagerInterface $manager): Response
-    {
-
+    #[Route("/contact", name: "app_contact")]
+    public function index(
+        Request $request,
+        EntityManagerInterface $manager,
+        MailerInterface $mailer
+    ): Response {
         $contact = new Contact();
 
         if ($this->getUser()) {
-            $contact->setFirstName($this->getUser()->getFirstName())
+            $contact
+                ->setFirstName($this->getUser()->getFirstName())
                 ->setLastName($this->getUser()->getLastName())
                 ->setEmail($this->getUser()->getEmail());
         }
@@ -27,7 +33,7 @@ class ContactController extends AbstractController
         $form = $this->createForm(ContactType::class, $contact);
 
         $form->handleRequest($request);
-        
+
         if ($form->isSubmitted() && $form->isValid()) {
             $contact = $form->getData();
 
@@ -35,15 +41,34 @@ class ContactController extends AbstractController
 
             $manager->flush();
 
-            $this->addFlash(
-                'success',
-                'Votre demande a bien été envoyer'
-             );
-             return $this->redirectToRoute('app_contact');
+            // EMAIL
+
+            $email = (new Email())
+                ->from("hello@example.com")
+                ->to("you@example.com")
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject("Time for Symfony Mailer!")
+                ->text("Sending emails is fun again!")
+                ->html(
+                    "<p>See Twig integration for better HTML integration!</p>"
+                );
+
+                try {
+                    $mailer->send($email);
+                } catch (TransportExceptionInterface $e) {
+                    // some error prevented the email sending; display an
+                    // error message or try to resend the message
+                }
+
+            $this->addFlash("success", "Votre demande a bien été envoyer");
+            return $this->redirectToRoute("app_contact");
         }
 
-        return $this->render('contact/index.html.twig', [
-            'form' => $form->createView(),
+        return $this->render("contact/index.html.twig", [
+            "form" => $form->createView(),
         ]);
     }
 }
